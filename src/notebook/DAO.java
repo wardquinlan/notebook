7618cc22600464532dc7ee0e22bcab87e93f30d4
Ward Quinlan <wardquinlan@outlook.com>
2024-04-15 09:28:10 -0400

    continue

---
+++ src/notebook/DAO.java
@@ -0,0 +1,26 @@
+package notebook;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class DAO {
+  private static Log log = LogFactory.getFactory().getInstance(DAO.class);
+  private Connection conn;
+  
+  public void connect(String host, String database, String username, String password) throws Exception {
+    if (conn != null) {
+      throw new Exception("already connected to datastore");
+    }
+    Class.forName("org.postgresql.Driver");
+    String url = "jdbc:postgresql://" + host + "/" + database + "?user=" + username;
+    log.info("connecting to database using " + url);
+    if (password != null) {
+      url = url + "&password=" + password;
+    }
+    conn = DriverManager.getConnection(url);  
+  }
+  
+}
